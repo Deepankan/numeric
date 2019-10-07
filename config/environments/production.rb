4afb4971b2ca4eb3fef0832209b46c93ab540212
Deepankan Keepworks <deepankan@keepworks.com>
2019-10-08 00:19:48 +0530

    Change mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'numeric-api.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://numeric-api.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
