751fa66f574dcc92ba62a3a001ea69abdc5ac529
Deepankan Keepworks <deepankan@keepworks.com>
2019-10-08 02:00:09 +0530

    Smtp changed to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,13 +94,20 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://numeric-api.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            ENV['EMAIL'],
-  password:             ENV['PASSWORD'],
-  authentication:       'plain'
+  # address:              'smtp.gmail.com',
+  # port:                 587,
+  # domain:               'gmail.com',
+  # user_name:            ENV['EMAIL'],
+  # password:             ENV['PASSWORD'],
+  # authentication:       'plain'
   # enable_starttls_auto: true
   # ^ ^ remove this option ^ ^
+  address:               'smtp.sendgrid.net',
+  port:                  '587',
+  authentication:        :plain,
+  user_name:             ENV['EMAIL'],
+  password:              ENV['PASSWORD'],
+  domain:                'https://numeric-api.herokuapp.com',
+  enable_starttls_auto:  true
 }
 end
